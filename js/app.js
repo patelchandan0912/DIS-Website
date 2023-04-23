5140d5554385ee02c34897825114fb3065f99503
khuranaraghav <khurana3@usf.edu>
2023-04-23 10:05:38 -0400

    raghav changes as of 10:05 am

--- js/app.js
+++ js/app.js
@@ -6,3 +6,15 @@ menuItems.forEach((item, index) => {
         wrapper.style.transform = `translateX(${-100 * index}vw)`;
     });
 });
+
+const images = document.querySelectorAll('.slider img');
+
+let index = 0;
+
+function changeImage() {
+  images[index].classList.remove('active');
+  index = (index + 1) % images.length;
+  images[index].classList.add('active');
+}
+
+setInterval(changeImage, 3000);
