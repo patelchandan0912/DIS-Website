580714f60c32f3ef2509e5ac7c6cb7db9de9276e
patelchandan0912 <chandanpatel@usf.edu>
2023-04-23 12:12:52 -0400

    Rev-1, updated codes and cleaned

--- js/app.js
+++ js/app.js
@@ -1,11 +1,3 @@
-const wrapper = document.querySelector(".sliderWrapper");
-const menuItems = document.querySelectorAll(".category");
-
-menuItems.forEach((item, index) => {
-    item.addEventListener ("click", () => {
-        wrapper.style.transform = `translateX(${-100 * index}vw)`;
-    });
-});
 
 const images = document.querySelectorAll('.slider img');
 
